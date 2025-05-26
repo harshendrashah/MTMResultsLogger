27d00ef246c21ed39e6d819942c60c8f52c23ac1
Harshendra Shah <harshendrashah812@gmail.com>
2025-05-26 12:34:15 +0530

    Updated runner script

--- localRunnerScript.m
+++ localRunnerScript.m
@@ -1,7 +1,9 @@
 import matlab.unittest.TestSuite
 import matlab.unittest.TestRunner
 
+addpath('src')
 suite = TestSuite.fromFolder("tests");
 runner = TestRunner.withNoPlugins;
 runner.addPlugin(ciplugins.qualitycenter.TestResultsViewPlugin);
 result = runner.run(suite);
+rmpath('src')
